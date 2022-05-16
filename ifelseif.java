b70c9e65fc92c98c00f7f67ee20d303924933ccc
InstituteDude <105227194+InstituteDude@users.noreply.github.com>
2022-05-16 21:00:38 +0700

    click me :)
    oke guys kali ini saya belajar tentang if else if, if else if ini cukup menarik karena kita bisa menambah lebih banyak statement dan tidak cukup dengan satu saja.
    *note : saya belajar dari kang pukis (kelas terbuka) di youtube

---
+++ ifelseif.java
@@ -0,0 +1,24 @@
+public class ifelseif {
+    public static void main(String[]args){
+        int a=90;
+
+        System.out.println("Welcome to the jungle and lets begini our journey");
+        
+        //if else if statement
+        if(a == 90){
+            System.out.println("you are right friend-!");
+        }else if(a == 10 ){
+            System.out.println("whaaatt are you a doctor strange?");
+        }else{
+            System.out.println("aha this time you are wrong ;)");
+        }
+
+         //end of the if else if statement boy
+
+            System.out.println("This is the end of your journey");
+
+
+
+
+    }
+}
